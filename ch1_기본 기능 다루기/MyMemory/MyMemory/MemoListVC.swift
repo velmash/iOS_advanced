c0c9ceeccbf7f665dbf7895d73fa0655918484aa
hyengchan <hyengchan@gmail.com>
2021-01-25 15:36:52 +0900

    메모장 앱 제작

---
+++ ch1_기본 기능 다루기/MyMemory/MyMemory/MemoListVC.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+class MemoListVC: UITableViewController {
+
+    // 앱 델리게이트 객체의 참조 정보를 읽어온다.
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    // 디바이스 스크린에 뷰 컨트롤러가 나타날 때마다 호출되는 메소드
+    override func viewWillAppear(_ animated: Bool) {
+        // 테이블 데이터를 다시 읽어들인다. 이에 따라 행을 구성하는 로직이 다시 실행될 것이다.
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = self.appDelegate.memolist.count
+        return count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 1. memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // 2. 이미지 속성이 비어 있을 경우 "memoCell", 아니면 "memoCellWithImage"
+        let cellId = row.image == nil ? "memoCell" : "memoCellWithImage"
+        
+        // 3. 재사용 큐로부터 프로포타입 셀의 인스턴스를 전달받는다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        // 4. memoCell의 내용을 구성한다.
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        // 5. Date 타입의 날짜를 yyyy-MM-dd HH:mm:ss 포맷에 맞게 변경한다.
+        // 개별적인 포맷 형식은 http://www.codingexplorer.com/swiftly-getting-human-readable-date-nsdateformatter/ 참고
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+                
+        cell.regdate?.text = formatter.string(from: row.regdate!)
+        
+        // 6. cell 객체를 리턴한다.
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // ① memolist 배열에서 선택된 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // ② 상세 화면의 인스턴스를 생성한다.
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadVC else {
+            return
+        }
+        
+        // ③ 값을 전달한 다음, 상세 화면으로 이동한다.
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
