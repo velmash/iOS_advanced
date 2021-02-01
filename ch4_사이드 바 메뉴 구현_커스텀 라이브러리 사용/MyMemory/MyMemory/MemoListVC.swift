f75845178bddc4e3f91a9dcc820637231fd514f4
hyengchan <hyengchan@gmail.com>
2021-02-01 09:25:40 +0900

    메모장 어플에 사이드바 추가

---
+++ ch4_사이드 바 메뉴 구현_커스텀 라이브러리 사용/MyMemory/MyMemory/MemoListVC.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+class MemoListVC: UITableViewController {
+    
+    // 앱 델리게이트 객체의 참조 정보를 읽어온다.
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        // SWRevealViewController 라이브러리의 revealViewController 객체를 읽어온다
+        if let revealVC = self.revealViewController() {
+            // 바 버튼 아이템 객체를 정의한다.
+            let btn = UIBarButtonItem()
+            btn.image = UIImage(named: "sidemenu.png") // 이미지는 sidemenu.png로
+            btn.target = revealVC // 버튼 클릭 시 호출할 메소드가 정의된 객체를 지정
+            btn.action = #selector(revealVC.revealToggle(_:)) // 버튼 클릭 시 revealToggle(_:) 호출
+            // 정의된 바 버튼을 내비게이션 바의 왼쪽 아이템으로 등록
+            self.navigationItem.leftBarButtonItem = btn
+            
+            // 제스처 객체를 뷰에 추가한다.
+            self.view.addGestureRecognizer(revealVC.panGestureRecognizer())
+        }
+    }
+    
+    // 디바이스 스크린에 뷰 컨트롤러가 나타날 때마다 호출되는 메소드
+    override func viewWillAppear(_ animated: Bool) {
+        // 테이블 데이터를 다시 읽어들인다. 이에 따라 행을 구성하는 로직이 다시 실행될 것이다.
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = self.appDelegate.memolist.count
+        return count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 1. memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // 2. 이미지 속성이 비어 있을 경우 "memoCell", 아니면 "memoCellWithImage"
+        let cellId = row.image == nil ? "memoCell" : "memoCellWithImage"
+        
+        // 3. 재사용 큐로부터 프로포타입 셀의 인스턴스를 전달받는다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        // 4. memoCell의 내용을 구성한다.
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        // 5. Date 타입의 날짜를 yyyy-MM-dd HH:mm:ss 포맷에 맞게 변경한다.
+        // 개별적인 포맷 형식은 http://www.codingexplorer.com/swiftly-getting-human-readable-date-nsdateformatter/ 참고
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        cell.regdate?.text = formatter.string(from: row.regdate!)
+        
+        // 6. cell 객체를 리턴한다.
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // ① memolist 배열에서 선택된 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // ② 상세 화면의 인스턴스를 생성한다.
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadVC else {
+            return
+        }
+        
+        // ③ 값을 전달한 다음, 상세 화면으로 이동한다.
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
